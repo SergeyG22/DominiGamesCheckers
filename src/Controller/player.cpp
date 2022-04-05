eb5c5661577d250a9ee821531b74872a6140ed45
SergeyGavrilov <46799640+SergeyG22@users.noreply.github.com>
2022-04-05 17:24:10 +0300

    Update player.cpp

--- src/Controller/player.cpp
+++ src/Controller/player.cpp
@@ -1,10 +1,11 @@
-#include "../../include/Model/player.h"
+#include "../../include/Controller/player.h"
 
-Player::Player(std::string name) : m_name(name) {
+Player::Player(std::string name, std::shared_ptr<Board> model_ptr) : m_name(name), m_model_ptr(model_ptr) {
 
 }
-
 
 std::string Player::getName() const {
     return m_name;
 }
+
+int Player::player_id = 0;
